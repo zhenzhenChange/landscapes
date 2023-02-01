5bf837f568458965c58022357f795065eafdd867
zhongzhen <1506678296@qq.com>
2023-02-01 11:58:28 +0800

    ⚗️ Haskell: example-rwh-02 updated

--- ProgrammingLanguage-Haskell/examples/rwh/02.hs
+++ ProgrammingLanguage-Haskell/examples/rwh/02.hs
@@ -1,2 +1,7 @@
 -- (!!)  -> List index (subscript) operator.
 penultimate xs = xs !! max 0 (length xs - 2)
+
+penultimateMatch [] = error "Empty"
+penultimateMatch [_] = error "Single"
+penultimateMatch [x, _] = x
+penultimateMatch (_ : xs) = penultimateMatch xs
