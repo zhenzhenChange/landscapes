a62c5d4250d9e7040e951b85589d4e7829f85f86
zhongzhen <1506678296@qq.com>
2023-02-03 16:37:40 +0800

    ⚗️ Haskell: example-rwh-04 updated: use as-pattern

--- ProgrammingLanguage-Haskell/examples/rwh/04-A-02.hs
+++ ProgrammingLanguage-Haskell/examples/rwh/04-A-02.hs
@@ -1,7 +1,7 @@
 splitWhile :: (t -> Bool) -> [t] -> [[t]]
 splitWhile predicate [] = []
-splitWhile predicate (x : xs)
+splitWhile predicate list@(x : xs)
   | predicate x =
-      let (pre, suf) = span predicate (x : xs)
+      let (pre, suf) = span predicate list
        in pre : splitWhile predicate suf
   | otherwise = splitWhile predicate xs
